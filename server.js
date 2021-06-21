1e7bc24d25be821fc80ceeb827f37f59c903a91b
Mohit <mohitsrt123@gmail.com>
2021-06-21 11:48:55 +0530

    errors removed

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const io=require('socket.io')(server)
 const {v4: uuidV4 }=require('uuid')
 
 app.set('view engine','ejs')
+
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-res.redirect('/${uuidV4()}')
+    res.redirect(`/${uuidV4()}`)
 })
+
 app.get('/:room',(req,res)=>{
     res.render('room',{roomId: req.params.room})
 })
 
 io.on('connection',socket =>{
     socket.on('join-room', (roomId, userId) =>{
+        console.log(roomId,userId)
         socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
     })
 })
 server.listen(3000)
