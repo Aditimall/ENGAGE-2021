1f28b2b7ac2fe165abe834db8fa490af4dba9078
Mohit <mohitsrt123@gmail.com>
2021-06-23 13:08:23 +0530

    final

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: 'https://video-call-engage.herokuapp.com/',
-    port: '3001'
+    port: '9000'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server=require('http').Server(app)
 const io=require('socket.io')(server)
 const {v4: uuidV4 }=require('uuid')
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 3001, path: 'https://video-call-engage.herokuapp.com/' });
+const peerServer = PeerServer({ port: 9000, path: 'https://video-call-engage.herokuapp.com/' });
 
 app.set('view engine','ejs')
 
