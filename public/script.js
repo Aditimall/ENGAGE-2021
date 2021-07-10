f666a9aa3c735aa9f4edef729e6d13ccce5a8edd
Ayush Thakur <76520218+ayush-thakur@users.noreply.github.com>
2021-07-10 13:09:10 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,13 +8,14 @@ const message=document.getElementById('message');
 myVideo.muted = true
 const name=prompt('Name')
 const peers = {}
-
+let myVideoStream;
 const mic=document.getElementById('mic')
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream =>{
+    myVideoStream = stream;
     addVideoStream(myVideo, stream)
     socket.on('user-connected',userId => { 
         connectToNewUser(userId, stream)
@@ -76,3 +77,58 @@ function addVideoStream(video, stream){
     })
     videoGrid.append(video)
 }
+
+const playStop = () => {
+    console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  }
+
+  const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+  }
+
+  const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  }
+
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
